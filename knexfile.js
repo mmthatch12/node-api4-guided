3218aa81069bc049cb9cdafccea66a282f3bb7a0
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-09-05 11:36:59 -0600

    added production to knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -17,4 +17,23 @@ module.exports = {
       directory: './data/seeds',
     },
   },
+  
+  production: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: './data/shouts.db3',
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
+    },
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+  },
 };
